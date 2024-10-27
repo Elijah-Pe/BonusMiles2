67a92aae5c2a2a76b92f1e2d770832e355cd039a
Ilya <ilia.petro2014@yandex.ru>
2024-10-27 17:28:26 +0400

    Add code and Jclass

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        BonusMilesService service = new BonusMilesService();
+        int price = 11_035;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
